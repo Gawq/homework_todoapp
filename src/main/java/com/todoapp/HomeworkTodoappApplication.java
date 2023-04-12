b1b40a6f9294a795bf247fc36114f58249a8b613
aaa <cnj991279@gmail.com>
2023-04-12 14:51:09 +0900

    initial

---
+++ src/main/java/com/todoapp/HomeworkTodoappApplication.java
@@ -0,0 +1,13 @@
+package com.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkTodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkTodoappApplication.class, args);
+	}
+
+}
